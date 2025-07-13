b0e4019ac1af814f560426a268527c8aa729c361
Omar Fostok <omar.fostok@ovrlab.io>
2025-07-13 07:58:36 +0300

    commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+import joblib
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+model = SentenceTransformer(f'quora_mpnet_v2_tuned_v3')
+docs_index = faiss.read_index(f'quora_faiss_index_v3.index')
+doc_ids = joblib.load(f"all_quora_doc_ids_v3.joblib")
+docs = joblib.load(f"all_quora_doc_texts.joblib")
+doc_embeddings = joblib.load(f"all_quora_doc_embeddings_v3.joblib") 
+
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+def search_without_faiss(dataset, query, top_k=5):
+    # Encode query
+    model = SentenceTransformer(f'{dataset}_mpnet_v2_tuned_v3')
+    query_embedding = model.encode([query])
+    query_embedding_np = np.array(query_embedding).astype('float32')
+
+    # Load docs adn docs ids
+    doc_embeddings = np.array(doc_embeddings).astype('float32')
+    
+    # Compute cosine similarities
+    similarities = cosine_similarity(query_embedding, doc_embeddings)[0]  # shape: (num_docs,)
+
+    # Get top-k indices
+    top_indices = np.argsort(similarities)[::-1][:top_k]
+
+    # Collect top-k results
+    results = []
+    for idx in top_indices:
+        results.append({
+            "doc_id": doc_ids[idx],
+            "text": docs[idx],
+            "score": float(similarities[idx])
+        })
+    return results
+
+
+# search function
+def search_faiss(dataset, query, top_k=5):
+    # Encode query
+    query_embedding = model.encode([query])
+    query_embedding_np = np.array(query_embedding).astype('float32')
+
+    # Load Faiss, docs, docs_ids
+
+    # Search in FAISS index
+    D, I = docs_index.search(query_embedding_np, top_k)
+
+    results = []
+    for score, idx in zip(D[0], I[0]):
+        results.append({
+            "doc_id": doc_ids[idx],
+            "text": docs[idx],
+            "score": 1 - float(score)
+        })
+    return results
+
+
+@app.post("/search")
+def search(query: str = Form(...), algorithm: str = Form(...)):
+    results = search_faiss("quora",query, top_k=10)
+    return {"results": results}
+
+
+
